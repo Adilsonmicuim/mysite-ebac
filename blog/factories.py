6ff02033e0a2c8492db1f08d2c21369a66377c7f
Adilson Lima de Paula <adilsonmicuim@gmail.com>
2023-04-08 14:08:13 -0300

    alteracoes no codigo

--- blog/factories.py
+++ blog/factories.py
@@ -1,8 +1,15 @@
 # Importações
+from multiprocessing.spawn import prepare
+
 import factory
 from django.contrib.auth.models import User
-from blog.models import Post
+from django.utils.timezone import now
 
+from blog.models import Post
+from faker import Factory as FakerFactory
+
+
+faker = FakerFactory.create()
 
 # Factory para User
 class UserFactory(factory.django.DjangoModelFactory):
@@ -24,3 +31,30 @@ class PostFactory(factory.django.DjangoModelFactory):
     author = factory.SubFactory(UserFactory)
     content = 'Test content'
     status = 0
+
+
+# class UserFactory(factory.django.DjangoModelFactory):
+#     class Meta:
+#         model = User
+#
+#     email = factory.Faker("safe_email")
+#     username = factory.LazyAttribute(lambda x: faker.name())
+#
+#     @classmethod
+#     def _prepare(cls, create, **kwargs):
+#         password = kwargs.pop("password", None)
+#         user = super(UserFactory, cls), prepare(create, **kwargs)
+#         if password:
+#             user.set_password(password)
+#             if create:
+#                 user.name()
+#         return user
+#
+#     class PostFactory(factory.django.DjangoModelFactory):
+#         title = factory.LazyAttribute(lambda x: faker.sentence())
+#         created_on = factory.LazyAttribute(lambda x: now())
+#         author = factory.SubFactory(UserFactory)
+#         status = 0
+#
+#         class Meta:
+#             model = Post
